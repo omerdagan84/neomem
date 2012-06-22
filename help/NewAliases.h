433e727d49c7dc242c7d57b6531ab27b0078391a
bburns <bburns@3bad96a7-2d8f-4f48-a7ae-748201937164>
2012-06-22 21:14:14 +0000

    better htmlhelp filenames
    git-svn-id: svn://localhost/neomemC/trunk@340 3bad96a7-2d8f-4f48-a7ae-748201937164

---
+++ help/NewAliases.h
@@ -0,0 +1,6 @@
+; Alias File written automatically by HHPMod Version 3.2.2
+; June 22, 2012, 03:50 PM
+; Automatic aliases for normal topics ;;;;;;;;;;;;;;;;;;;;;;;;;;
+HID_KEYBOARD_SHORTCUTS = html\HID_KEYBOARD_SHORTCUTS.htm
+HID_WHATS_NEW = html\HID_WHATS_NEW.htm
+HID_CLOCK = html\HID_CLOCK.htm
---
+++ help/popups.js
@@ -0,0 +1,5 @@
+// Javascript Popups File written automatically by HHPMod Version 3.2.2
+// June 22, 2012, 03:50 PM
+
+popfont = "Arial,10,,plain"
+
